35115d5c7cb2fff334ddf8c53165415e9605818e
Elena Komarova <geta1991@mail.ru>
2020-05-13 17:28:21 +0500

    checking the add to cart button

---
+++ test_items.py
@@ -0,0 +1,5 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_add_to_cart_button(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("button.btn.btn-lg.btn-primary.btn-add-to-basket")
