985da1c5035c642407309c3ab1fc90fa2771ded3
Elena Komarova <geta1991@mail.ru>
2020-05-13 18:21:44 +0500

    deleted test_items1.py

--- test_items1.py
+++
@@ -1,8 +0,0 @@
-import time
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
-def test_add_to_cart_button(browser):
-    browser.get(link)
-    time.sleep(30)
-    btn = browser.find_element_by_css_selector("button.btn.btn-lg.btn-primary.btn-add-to-basket")
-    assert btn
