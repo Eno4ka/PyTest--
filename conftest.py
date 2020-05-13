363cd588e3e2e792c39c8f33dd0bb48e7d1efdaf
Elena Komarova <geta1991@mail.ru>
2020-05-13 18:04:33 +0500

    handler with the language parameter

--- conftest.py
+++ conftest.py
@@ -4,22 +4,15 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="ru")
+    parser.addoption('--language', action='store', default="ru", help='Choose language')
 
 
 @pytest.fixture(scope="function")
 def browser(request):
+    user_language = request.config.getoption('language')
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': "es"})
-    language = request.config.getoption("language")
-    if language == "ru":
-        print("\nstart ru language for test..")
-        browser = webdriver.Chrome(options=options)
-    elif language == "es":
-        print("\nstart es language for test..")
-        browser = webdriver.Chrome(options=options)
-    else:
-        raise pytest.UsageError("--language should be ru or es")
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(5)
     yield browser
-    print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,5 +1,8 @@
+import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_to_cart_button(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("button.btn.btn-lg.btn-primary.btn-add-to-basket")
+    time.sleep(30)
+    btn = browser.find_element_by_css_selector("button.btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert btn
